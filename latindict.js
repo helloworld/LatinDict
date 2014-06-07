a3cbc12bff5c940b07769ca5a72111472c6ab405
Sashank Thupukari <sthupukari@gmail.com>
2014-06-07 12:58:24 -0400

    New mobile view using individual cards.
    Table looks very nice on mobile screens. Words display as individual
    cards and user can scroll through them vertically.
    
    Also added comments for js.

--- latindict.js
+++ latindict.js
@@ -1,20 +1,24 @@
+//Create a new collection for all dictionary entries. 
 Dictionary = new Meteor.Collection('dictionary');
 
 
 if (Meteor.isClient) {
 
+    //Subscribes to the dicionary collection, which is published in the 
+    //Meteor server methods. 
     dictionarySub = Meteor.subscribe('dictionary');
 
     Template.DictionaryPanel.helpers({
+
+        //Handlebar helper that returns items as the 'searchedWords' session, 
+        //variable, which is generated in the Dictionary panel events and 
+        //contains the matching results for the user's search input.
         items: function() {
             return Session.get("searchedWords");
-            // return Dictionary.find({}, {
-            //     sort: {
-            //         created_at: -1
-            //     }
-            // });
         },
 
+        //Boolean handlebar helpers to sort words by type when 
+        //dislayed in the table.
         isVerb: function() {
             return (this.type == "verb");
         },
@@ -24,9 +28,14 @@ if (Meteor.isClient) {
         isAdjective: function() {
             return (this.type == "adjective");
         }
+
     });
 
+
     Template.DictionaryPanel.events({
+
+        //Creates an array with matching search words everytime the user 
+        //types into the search field
         'keyup': function(e, tmpl) {
             var searchWord = $('#searchWord').val().toLowerCase();
             var array = Dictionary.find({
@@ -39,7 +48,8 @@ if (Meteor.isClient) {
                     word: 1
                 }
             }).fetch();
-            console.log(searchWord);
+
+            //If the search field is blank, all words placed into the array.
             if (searchWord == "") {
                 array = Dictionary.find({}, {
                     sort: {
@@ -47,6 +57,9 @@ if (Meteor.isClient) {
                     }
                 }).fetch();
             }
+
+            //Sets the session variable 'searchedWords' to the array 
+            //generated above
             Session.set('searchedWords', array);
 
         }
@@ -56,6 +69,8 @@ if (Meteor.isClient) {
 
     Template.DictionaryItem.helpers({
 
+        //Boolean handlebar helpers to sort words by type when 
+        //dislayed in the table. 
         isVerb: function() {
             return (this.type == "verb");
         },
@@ -71,27 +86,36 @@ if (Meteor.isClient) {
 
 
     Template.CreateDictionaryItem.events({
+
+        //Takes input from admin interface, and assigns them to variables.  
         'submit form': function(e, tmpl) {
             e.preventDefault();
             var word = tmpl.find('input[name=word]').value;
             var definition = tmpl.find('input[name=definition]').value;
             var type = tmpl.find('input[name=type]').value;
             var gender = tmpl.find('input[name=gender]').value;
+            var conj = tmpl.find('input[name=conj]').value;
 
+            //Takes variables above and creates a item and adds the item
+            //to the Dictionary collection.
             Dictionary.insert({
                 word: word,
                 definition: definition,
                 type: type,
                 gender: gender,
+                conj: conj,
                 created_at: new Date
             });
 
+            //Resets form once item is added to the Dictionary.
             var form = tmpl.find('form');
             form.reset();
         }
     });
 
     Template.DictionaryCount.helpers({
+
+        //Returns the total number of items in the Dictionry collection
         totalCount: function() {
             return Dictionary.find({}).count();
         }
@@ -100,10 +124,13 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
+    //Manually publishes the Dictionary collection with all the entries included.
     Meteor.publish('dictionary', function() {
         return Dictionary.find({});
     });
 
+    //Set permissions for adding, removing, and updating words.
     Dictionary.allow({
         insert: function(userId, doc) {
             return userId;
